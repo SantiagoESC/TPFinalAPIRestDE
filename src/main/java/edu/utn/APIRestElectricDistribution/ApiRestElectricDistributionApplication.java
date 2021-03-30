97c3e6e0cf74bf9a5ef9532b5c7540900b5e6646
SantiagoESC <48869961+SantiagoESC@users.noreply.github.com>
2021-03-29 21:55:07 -0300

    First Commit

---
+++ src/main/java/edu/utn/APIRestElectricDistribution/ApiRestElectricDistributionApplication.java
@@ -0,0 +1,13 @@
+package edu.utn.APIRestElectricDistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestElectricDistributionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestElectricDistributionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utn/APIRestElectricDistribution/ApiRestElectricDistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.APIRestElectricDistribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestElectricDistributionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
