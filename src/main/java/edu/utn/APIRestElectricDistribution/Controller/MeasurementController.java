d35c6631d8fe103d679c8e2d252ff6c5643c2063
Franco Acebo Canal <48834868+FrancoAceboC@users.noreply.github.com>
2021-04-07 18:10:08 -0300

    measurementController added but commented

--- src/main/java/edu/utn/APIRestElectricDistribution/Controller/MeasurementController.java
+++ src/main/java/edu/utn/APIRestElectricDistribution/Controller/MeasurementController.java
@@ -1,9 +1,31 @@
 package edu.utn.APIRestElectricDistribution.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.utn.APIRestElectricDistribution.Domain.Measurement;
+import edu.utn.APIRestElectricDistribution.Service.MeasurementService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/measurement/")
 @RestController
 public class MeasurementController {
+
+//    //Properties region
+//    private final MeasurementService measurementService;
+//
+//    //Constructor region
+//    public MeasurementController(MeasurementService measurementService) {
+//        this.measurementService = measurementService;
+//    }
+//
+//    //Get region
+//    @GetMapping("/")
+//    private List<Measurement>FindAll(){
+//        return this.measurementService.GetAll();
+//    }
+//
+//    //FOR ID
+//
+//    //Post region
+//    @PutMapping()
 }
