5ff9ee41e63795883c1063df1f0b21d609388a65
Franco Acebo Canal <48834868+FrancoAceboC@users.noreply.github.com>
2021-05-24 13:55:30 -0300

    Working with the DB

--- src/main/resources/schema/mysql/APIRestElectricDistribution.sql
+++ src/main/resources/schema/mysql/APIRestElectricDistribution.sql
@@ -2,15 +2,17 @@ create database electric_distribution;
 use electric_distribution;
 set global time_zone = '-3:00';
 
+
+ ----------------------------------------------TABLES -------------------------------------------------------
 create table users(
 
     id_user bigint auto_increment,
-    first_name varchar(40),
-    last_name varchar(40),
+    first_name varchar(40) not null,
+    last_name varchar(40) not null,
     ID_card_number varchar(20) not null ,
     password varchar(30) not null,
     enabled tinyint not null default 1,
-    user_role enum("CLIENT", "EMPLOYEE"),
+    user_role enum("CLIENT", "EMPLOYEE") not null,
     constraint pk_id_user primary key(id_user),
     constraint unq_id_card_number unique (ID_card_number)
 );
@@ -80,3 +82,146 @@ create table bills(
   constraint fk_id_electrical_measurement_initial foreign key (electrical_measurement_initial_id) references electrical_measurements(id_electrical_measurements),
   constraint fk_id_rate foreign key (id_rate) references rates(id_rate)
 );
+
+-------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------- INSERT DATA ---------------------------------------------------
+/*   ---------   USERS --------------- --------------------------------------------------*/
+insert into users(first_name, last_name, ID_card_number, password, enabled, user_role)
+            values ('Franco', 'Acebo', '123', '123', 1, 'CLIENT'),
+                   ('Santiago', 'Escribas', '456', '456', 1, 'EMPLOYEE');
+/*---------------------------------------------------------------------------------------*/
+
+/*----------  ADDRESS ---------------------------------------------------------------------*/
+insert into address(street_name, street_number, zip_code, city)
+            values('Paso', '1111', '7600', 'Mar del Plata'),
+                  ('Necochea', '3333', '7600', 'Mar del Plata');
+
+insert into address(street_name, street_number, floor, department, zip_code, city)
+            values ('Buenos Aires', '222', 3, 'D', '7600', 'Mar del Plata'),
+                   ('Calle 11', '209', 1, 'A', '7601', 'Batan');
+/* ------------------------------------------------------------------------------------------*/
+
+/*----------  ELECTRICAL METER ---------------------------------------------------------------------*/
+
+insert into electrical_meter(id_user, id_address, serial_number, brand, model)
+                    values (1, 1, 'A445DGFKN', 'Lux', 'AP-881E'),
+                            (1,2, 'AR3456GD', 'Emylo', '60-A');
+/* ------------------------------------------------------------------------------------------*/
+/*----------  ELECTRICAL MEASUREMENTS ---------------------------------------------------------------------*/
+insert into electrical_measurements(id_electrical_meter, measurement_date, measurement_kwh)
+                        values (1, '2021-01-15', 150),
+                               (2, '2021-02-15', 180);
+/* ------------------------------------------------------------------------------------------*/
+/*----------  RATES ---------------------------------------------------------------------*/
+insert into rates(type_rates, price)
+            values (),
+                   ();
+/* ------------------------------------------------------------------------------------------*/
+/*----------  BILLS ---------------------------------------------------------------------*/
+insert into bills(id_electrical_meter, electrical_measurement_initial_id, electrical_measurement_final_id, id_rate,
+                    date_bill, expiration_date, billed, total_price)
+            values (),
+                ();
+/* ------------------------------------------------------------------------------------------*/
+
+
+-- =================================================================
+--                          FUNCTIONS
+-- =================================================================
+
+/*Verifica que el string solo contenga enteros*/
+CREATE FUNCTION isNumeric (word varchar(1024))
+    RETURNS tinyint deterministic
+    RETURN word REGEXP '^-?[0-9]+$';
+
+/*Verifica que el string no contenga enteros*/
+CREATE FUNCTION conteinsNumbers ( word varchar(1024))
+    RETURNS tinyint deterministic
+    RETURN word REGEXP '-?[0-9]';
+
+-- ===============================================================
+--                      PROCEDURES
+-- ===============================================================
+
+/*UPDATE_USER*/
+DELIMITER $$
+CREATE PROCEDURE sp_update_user(pFirstName varchar(40), pLastName varchar (40), pID_card_number varchar(20), out pIdUser bigint)
+BEGIN
+    START TRANSACTION;
+        UPDATE users
+        SET first_name = pFirstName,
+            last_name = pLastName
+        WHERE ID_card_number = pID_card_number;
+        SELECT id_user INTO pIdUser FROM users WHERE ID_card_number = pId_card_number;
+    COMMIT;
+END $$;
+
+-- -----------------------------------------------------------------
+
+/* DELETE */
+
+DELIMITER $$
+CREATE PROCEDURE sp_delete_user(pID_card_number varchar(20))
+BEGIN
+    DECLARE vIdUser BIGINT;
+    START TRANSACTION;
+        UPDATE users
+        SET users.enabled = 0
+        WHERE users.ID_card_number = pID_card_number;
+
+        SELECT u.id_user INTO vIdUser FROM users u WHERE u.ID_card_number = pID_card_number;
+
+
+        /* ACA VA EL UPDATE DE ELECTRICAL METER
+
+         */
+
+    COMMIT;
+END $$;
+
+/* BILL */
+DELIMITER $$
+CREATE PROCEDURE sp_create_bill(pId_electrical_meter int)
+BEGIN
+    /* DECLARACIONES */
+
+
+    /*
+        LOGICA
+     */
+
+END $$;
+
+
+
+
+
+
+-- =================================================================
+--  TRIGGERS
+-- =================================================================
+
+/* Verificar que el ID Card number (DNI) contenga solo numeros y nombre/apellido solo letras */
+
+DELIMITER $$
+CREATE TRIGGER TBI_user BEFORE INSERT  ON users FOR EACH  ROW
+BEGIN
+
+    IF(!isNumeric(new.ID_card_number))then
+        signal sqlstate '10001'
+        SET MESSAGE_TEXT = 'The ID Card number has an incorrect format'
+        MYSQL_ERRNO = 4;
+    END IF;
+
+    IF(conteinsNumbers(new.firstName)) then
+        signal sqlstate '10001'
+		SET MESSAGE_TEXT = 'First Name has an incorrect format',
+		MYSQL_ERRNO = 5;
+    END IF;
+
+     IF(conteinsNumbers(new.lastName)) then
+            signal sqlstate '10001'
+            SET MESSAGE_TEXT = 'Last name has an incorrect format',
+            MYSQL_ERRNO = 5;
+    END IF;
+END $$;
